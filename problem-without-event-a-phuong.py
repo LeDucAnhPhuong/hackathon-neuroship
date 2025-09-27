070d854352892eff50f5aa8e19f6586804a82d5a
sawsew467 <thangtvb.dev@gmail.com>
2025-09-27 22:04:15 +0700

    speedup speed

--- problem-without-event-a-phuong.py
+++ problem-without-event-a-phuong.py
@@ -129,8 +129,8 @@ class JetBotController:
 
     def setup_parameters(self):
         self.WIDTH, self.HEIGHT = 300, 300
-        self.BASE_SPEED = 0.16
-        self.TURN_SPEED = 0.2
+        self.BASE_SPEED = 0.3
+        self.TURN_SPEED = 0.3
         self.TURN_DURATION_90_DEG = 0.8
         self.ROI_Y = int(self.HEIGHT * 0.85)
         self.ROI_H = int(self.HEIGHT * 0.15)
