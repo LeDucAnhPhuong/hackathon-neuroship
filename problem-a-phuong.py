fbb38d9c829a6f6759b0f9e295e04ba4502d69e3
sawsew467 <thangtvb.dev@gmail.com>
2025-09-27 18:41:45 +0700

    iiwiw

--- problem-a-phuong.py
+++ problem-a-phuong.py
@@ -142,8 +142,10 @@ class JetBotController:
         self.SAFE_ZONE_PERCENT = 0.3
         self.LINE_COLOR_LOWER = np.array([0, 0, 0])
         self.LINE_COLOR_UPPER = np.array([180, 255, 75])
-        self.INTERSECTION_CLEARANCE_DURATION = 5.3
-        self.INTERSECTION_APPROACH_DURATION = 1
+
+        self.INTERSECTION_CLEARANCE_DURATION = 5
+        self.INTERSECTION_APPROACH_DURATION = 1.2
+
         self.LINE_REACQUIRE_TIMEOUT = 3.0
         self.SCAN_PIXEL_THRESHOLD = 100
         self.YOLO_MODEL_PATH = "models/best.onnx"
