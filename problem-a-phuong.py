9e470a03d3038ca6d79caabd509a219dd970d241
sawsew467 <thangtvb.dev@gmail.com>
2025-09-27 18:54:03 +0700

    iiwiw

--- problem-a-phuong.py
+++ problem-a-phuong.py
@@ -143,7 +143,7 @@ class JetBotController:
         self.LINE_COLOR_LOWER = np.array([0, 0, 0])
         self.LINE_COLOR_UPPER = np.array([180, 255, 75])
 
-        self.INTERSECTION_CLEARANCE_DURATION = 5
+        self.INTERSECTION_CLEARANCE_DURATION = 1.5
         self.INTERSECTION_APPROACH_DURATION = 1.3
 
         self.LINE_REACQUIRE_TIMEOUT = 3.0
